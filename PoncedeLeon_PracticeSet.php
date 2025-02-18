b0ac24559b966ff83735d094f30b103d2684beb0
Teodore James M. Ponce de Leon <teodorejames@gmail.com>
2025-02-18 18:51:02 +0800

    Answered more questions

--- PoncedeLeon_PracticeSet.php
+++ PoncedeLeon_PracticeSet.php
@@ -7,6 +7,18 @@ class Person {
 $person = new Person();
 $person->name = 'Alice';
 $person->age = 25;
+$person->cashOnhand = 10.5;
+
+$Name = 'Brad';
+$Age = 40;
+$hasKids = 'true';
+$cashOnhand = 10.5;
+
+define("CONSTANT_NAME", "constant_value");
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PI', '3.1416');
+
 ?>
 
 <!DOCTYPE html>
@@ -24,5 +36,25 @@ $person->age = 25;
     <h2>Question 2: What is the output of <code>var_dump($person);</code>?</h2>
     <h3>Answer:
     <pre><?php var_dump($person); ?></pre></h3>
+
+    <h2>Question 3: What is the output of <code>var_dump($cashOnhand);</code>?</h2>
+    <h3>Answer:
+    <pre><?php var_dump($cashOnhand); ?></pre></h3>
+
+    <h2>Question 4: What character is used to concatenate strings?</h2>
+    <h3>
+    <pre><?php echo "$Name is $Age years old";
+               echo "${Name} is ${Age} years old";
+               echo '<h3>' . $Name . 'is' . $Age . 'years old</h3>';
+    ?></pre></h3>
+    <h3>Answer: "." / dot</h3>
+
+    <h2>Question 5: What is the output of <code>echo HOST?</code>?</h2>
+    <h3>Answer:
+    <pre><?php echo HOST; ?></pre></h3>
+
+    <h2>Question 6: How do you define in PHP the variable PI with value 3.1416?</code>?</h2>
+    <h3>Answer:
+    <pre><?php echo PI; ?></pre></h3>
 </body>
 </html>
