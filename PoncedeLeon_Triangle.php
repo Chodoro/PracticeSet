6b0124d60fc375550d2d7b07565b7e40479c33ee
Teodore James M. Ponce de Leon <teodorejames@gmail.com>
2025-02-18 19:12:49 +0800

    created form for triangle calculator

---
+++ PoncedeLeon_Triangle.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Triangle Area Calculator</title>
+</head>
+<body>
+
+<h2>Calculate Triangle Area using Heron's Formula</h2>
+<form method="POST">
+    Side 1: <input type="number" name="side1" required><br><br>
+    Side 2: <input type="number" name="side2" required><br><br>
+    Side 3: <input type="number" name="side3" required><br><br>
+    <input type="submit" name="calculate" value="Calculate Area">
+</form>
+</body>
+</html>
